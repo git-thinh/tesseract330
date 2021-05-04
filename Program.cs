93a3a902bc411f09af057e239dce7597c84120f1
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-05-04 21:12:29 +0700

    done

--- Program.cs
+++ Program.cs
@@ -12,36 +12,12 @@ using Newtonsoft.Json;
 
 class Program
 {
-    static int m_port_write = 0;
-    static int m_port_read = 0;
-
-    #region [ CODE ]
-
+    static int __PORT_WRITE = 1000;
+    static int __PORT_READ = 1001;
     static RedisBase m_subcriber;
-    static bool _subscribe(string channel)
-    {
-        if (string.IsNullOrEmpty(channel)) return false;
-        channel = "<{" + channel + "}>";
-        try
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.Append("*2\r\n");
-            sb.Append("$10\r\nPSUBSCRIBE\r\n");
-            sb.AppendFormat("${0}\r\n{1}\r\n", channel.Length, channel);
+    static bool __running = true;
 
-            byte[] buf = Encoding.UTF8.GetBytes(sb.ToString());
-            var ok = m_subcriber.SendBuffer(buf);
-            var lines = m_subcriber.ReadMultiString();
-            //Console.WriteLine("\r\n\r\n{0}\r\n\r\n", string.Join(Environment.NewLine, lines));
-            return ok;
-        }
-        catch (Exception ex)
-        {
-        }
-        return false;
-    }
-
-    static oTesseractRequest __ocrExecute(oTesseractRequest req, Bitmap image, RedisBase redis)
+    static oTesseractRequest __ocrExecute(oTesseractRequest req, Bitmap image)
     {
         PageIteratorLevel level = PageIteratorLevel.Word;
         switch (req.command)
@@ -93,7 +69,9 @@ class Program
                         req.ok = 1;
                         break;
                     default:
-                        var boxes = tes.GetSegmentedRegions(level).Select(x => new oTesseractBox(x)).ToArray();
+                        var boxes = tes.GetSegmentedRegions(level).Select(x =>
+                            string.Format("{0}_{1}_{2}_{3}", x.X, x.Y, x.Width, x.Height)).ToArray();
+                        req.output_format = "x_y_width_height";
                         req.output_text = string.Join("|", boxes.Select(x => x.ToString()).ToArray());
                         req.output_count = boxes.Length;
                         req.ok = 1;
@@ -114,8 +92,7 @@ class Program
             string json = redis.HGET("_OCR_REQUEST", guid);
             r = JsonConvert.DeserializeObject<oTesseractRequest>(json);
             Bitmap bitmap = redis.HGET_BITMAP(r.redis_key, r.redis_field);
-            if (bitmap != null)
-                r = __ocrExecute(r, bitmap, redis);
+            if (bitmap != null) r = __ocrExecute(r, bitmap);
         }
         catch (Exception ex)
         {
@@ -137,14 +114,10 @@ class Program
         }
     }
 
-    #endregion
-
     static void __startApp()
     {
-        if (m_port_write == 0) m_port_write = 1000;
-        if (m_port_read == 0) m_port_read = 1001;
         m_subcriber = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_SUBCRIBE, 1001));
-        _subscribe("__TESSERACT_IN");
+        m_subcriber.PSUBSCRIBE("__TESSERACT_IN");
 
         var bs = new List<byte>();
         while (__running)
@@ -158,7 +131,6 @@ class Program
                     new Thread(new ParameterizedThreadStart((o)
                         => __executeBackground((byte[])o))).Start(buf);
                 }
-
                 Thread.Sleep(100);
                 continue;
             }
@@ -168,7 +140,6 @@ class Program
         }
     }
 
-    static bool __running = true;
     static void __stopApp() => __running = false;
 
     #region [ SETUP WINDOWS SERVICE ]
